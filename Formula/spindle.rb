1a6c27287d62a8ceeffe53089116655ee6b29d7b
Ninad <25496560+spninad@users.noreply.github.com>
2026-02-22 23:19:14 -0800

    Fix formula: shorten desc and use valid SHA256 placeholders

--- Formula/spindle.rb
+++ Formula/spindle.rb
@@ -1,5 +1,5 @@
 class Spindle < Formula
-  desc "CLI tool for installing reusable source-code components and running project scripts"
+  desc "Source-code component installer with npm-like script runner"
   homepage "https://github.com/spninad/spindle"
   version "0.1.0"
   license "MIT"
@@ -7,25 +7,24 @@ class Spindle < Formula
   on_macos do
     on_arm do
       url "https://github.com/spninad/spindle/releases/download/v#{version}/spindle-#{version}-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
 
     on_intel do
       url "https://github.com/spninad/spindle/releases/download/v#{version}/spindle-#{version}-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_X86_64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/spninad/spindle/releases/download/v#{version}/spindle-#{version}-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86_64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   def install
     bin.install "spindle"
-    # Install the `sp` short alias
     bin.install_symlink bin/"spindle" => "sp"
   end
 
